10b50a3b5b08a7c962aabc8c2dc42b1aae44a66f
Rebecca Willison <rswillison@gmail.com>
2020-10-29 20:50:37 -0500

    initial commit for exploratory modeling script

---
+++ exploratory modeling/exploratory_analysis.R
@@ -0,0 +1,13 @@
+
+######################################################################
+#
+#  Script to do some exploratory analysis
+#
+######################################################################
+
+library(dplyr)
+library(ggplot2)
+library(leaflet)
+library(mapview)
+library(sp)
+library(lgcp)
