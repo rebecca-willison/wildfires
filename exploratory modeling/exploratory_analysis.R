981b7abf2d7445ffabad6619a20999aab4e3cc01
rebecca-willison <rswillison@gmail.com>
2020-10-30 00:57:52 -0500

    add filename

--- exploratory modeling/exploratory_analysis.R
+++ exploratory modeling/exploratory_analysis.R
@@ -10,4 +10,8 @@ library(ggplot2)
 library(leaflet)
 library(mapview)
 library(sp)
+library(spacetime)
 library(lgcp)
+
+### load dataset for modeling
+fires <- read.csv('data/ca_modeling_dataset.csv')
